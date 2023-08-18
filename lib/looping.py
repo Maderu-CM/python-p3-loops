8a1690e28d639acf0ae17166b7aaac75edd08963
MADERU-CM <cliv.moyia@student.moringaschool.com>
2023-08-18 12:20:49 +0300

    Assignment Done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
-def happy_new_year():
-    # code goes here!
-    pass
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+
+
+
+
+  
+
+
+
+
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+# Call the function to perform the FizzBuzz game
+fizzbuzz()
+
